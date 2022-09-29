4c8edbce3c4d57532c6deeb6e5f29ca76d942061
Daniele Severiano <danisck@gmail.com>
2022-09-29 19:06:24 -0300

    Como utilizar o método de requisição e resposta na web com PHP

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INSCRIÇÃO DE COMPETIDORES</title>
+</head>
+
+<body>
+    <h1>Formulário para Inscrição de Competidores</h1>
+    <!-- quando os dados são enviados pelo metodo get,
+    eles vão trafegar pelo header da
+    requisição, no formato query strings
+Ex: http://localhost/php/script.php?name=Daniele+Cosvosk+C.+Severiano&age=34 -->
+    <form action="script.php" method="post">
+        <div>
+            <label for="name">Seu Nome:</label>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label for="age">Sua Idade:</label>
+            <input type="text" name="age">
+        </div>
+        <input type="submit" value="Enviar dados do competidor">
+    </form>
+</body>
+
+</html>
--- script.php
+++ script.php
@@ -7,28 +7,29 @@ $Categorias[] = "Adulto";
 
 // print_r($Categorias);
 
-$nome = "Daniele";
-$idade = 34;
+$nome = $_POST["name"];
+$idade = $_POST["age"];
 
 // var_dump($nome);
 // var_dump($idade);
 
 if ($idade >= 6 && $idade <= 12) {
     for ($i = 0; $i <= count($Categorias); $i++) {
-        if($Categorias[$i] === "Infantil") {
+        if ($Categorias[$i] === "Infantil") {
             echo "O nadador {$nome} compete na categoria Infantil";
         }
     }
 } else if ($idade >= 13 && $idade <= 18) {
     for ($i = 0; $i <= count($Categorias); $i++) {
-        if($Categorias[$i] === "Adolescentes") {
+        if ($Categorias[$i] === "Adolescentes") {
             echo "O nadador {$nome} compete na categoria Adolescentes";
         }
     }
 } else {
     for ($i = 0; $i <= count($Categorias); $i++) {
-        if($Categorias[$i] === "Adulto") {
+        if ($Categorias[$i] === "Adulto") {
             echo "O nadador {$nome} compete na categoria Adulto";
         }
     }
 }
+?>
