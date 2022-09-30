a67b45fd4a2e7988d59af81b703549e827cd89e6
Daniele Severiano <danisck@gmail.com>
2022-09-30 14:48:43 -0300

    Introdução a funções em PHP

--- script.php
+++ script.php
@@ -13,29 +13,6 @@ $Categorias[] = "Adulto";
 $nome = $_POST["name"];
 $idade = $_POST["age"];
 
-if(empty($nome)) {
-    // vai criar um array de sessao (array associativo, atribui um valor a uma chave)
-    // com a chave 'mensagem de erro'
-    $_SESSION["mensagem de erro"] = "O nome não pode ser vazio";
-    header("Location: index.php");
-    return;
-
-} else if(strlen($nome) < 3) {
-    $_SESSION["mensagem de erro"] = "O nome deve conter mais que 3 caracteres";
-    header("Location: index.php");
-    return;
-
-} else if(strlen($nome) > 40) {
-    $_SESSION["mensagem de erro"] = "O nome é muito extenso!";
-    header("Location: index.php");
-    return;
-
-} else if(!is_numeric($idade)) {
-    $_SESSION["mensagem de erro"] = "Informe um número para idade";
-    header("Location: index.php");
-    return;
-}
-
 // var_dump($nome);
 // var_dump($idade);
 
---
+++ services/ServiceValidation.php
@@ -0,0 +1,27 @@
+<?php
+
+function nameValidation($nome) : bool
+{
+    if (empty($nome)) {
+        // vai criar um array de sessao (array associativo, atribui um valor a uma chave)
+        // com a chave 'mensagem de erro'
+        $_SESSION["mensagem de erro"] = "O nome não pode ser vazio";
+        return false;
+    } else if (strlen($nome) < 3) {
+        $_SESSION["mensagem de erro"] = "O nome deve conter mais que 3 caracteres";
+        return false;
+    } else if (strlen($nome) > 40) {
+        $_SESSION["mensagem de erro"] = "O nome é muito extenso!";
+        return false;
+    }
+    return true;
+}
+
+function ageValidation($idade)
+{
+    if (!is_numeric($idade)) {
+        $_SESSION["mensagem de erro"] = "Informe um número para idade";
+        header("Location: index.php");
+        return;
+    }
+}
